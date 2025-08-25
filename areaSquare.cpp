1e9777321af1f57f163fabcc397357a08a1db7ee
vivek.kambar18 <vivekkambar2409@gmail.com>
2025-08-25 19:40:59 +0530

    Add files via upload

---
+++ areaSquare.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float area,a;
+    cout<<"Give the dimentions of area of square to be calculate: ";
+    cin>> a;
+    area=a*a;
+    cout<<"area of square of dimnetion  "<<a<<" is: "<<area;
+    return 0;
+}
