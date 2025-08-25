b362ddd36aa3dc9a81834bb2c674637dedd75b4f
vivek.kambar18 <vivekkambar2409@gmail.com>
2025-08-25 20:06:44 +0530

    Add files via upload

---
+++ oddOReven.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    while(1){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    if(n%2==0){
+        cout<<"The number is even";
+    }
+    else{
+        cout<<"The number is odd";
+    }
+    cout<<endl;
+}
+}
