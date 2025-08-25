dcbc332311b02c5d8db1e9a3994016c83f126b62
vivek.kambar18 <vivekkambar2409@gmail.com>
2025-08-25 19:21:23 +0530

    Add files via upload

---
+++ twoaum.cpp
@@ -0,0 +1,10 @@
+//Adding two sum
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    int sum=a+b;
+    cout<<"The sum is: "<<sum;
+}
